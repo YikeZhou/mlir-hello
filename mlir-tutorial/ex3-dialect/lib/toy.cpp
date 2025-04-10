277d7d8c30a8ef4ec1a7ed7be82e14d9214ddc2a
Yike Zhou <yikezhou@outlook.com>
2025-04-10 17:58:33 +0800

    Add ex3-dialect example
    Introduce a new example in the mlir-tutorial project by adding an ex3-dialect directory. This includes the implementation of a new Toy dialect with operations such as add, sub, and const, along with the necessary CMake configuration and source files for building the dialect and its associated tools.

---
+++ mlir-tutorial/ex3-dialect/lib/toy.cpp
@@ -0,0 +1,17 @@
+#include "toy/ToyDialect.h"
+#include "toy/ToyOps.h"
+
+#include "toy/ToyDialect.cpp.inc"
+#define GET_OP_CLASSES
+#include "toy/Toy.cpp.inc"
+
+using namespace mlir;
+using namespace toy;
+
+void ToyDialect::initialize() {
+  // 下面的代码会生成 Op 的列表，专门用来初始化
+  addOperations<
+#define GET_OP_LIST
+#include "toy/Toy.cpp.inc"
+  >();
+}
