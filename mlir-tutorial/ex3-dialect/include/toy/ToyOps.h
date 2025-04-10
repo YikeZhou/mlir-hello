5af5bd7c5a774a113824417a34ca7ec9f4e8474f
Yike Zhou <yikezhou@outlook.com>
2025-04-10 20:54:29 +0800

    Add header guards and include necessary interfaces in Toy dialect files
    Enhance the Toy dialect by adding header guards to Toy.td and ToyDialect.td, and including additional MLIR interfaces in ToyOps.h and ToyOps.td. Introduce a custom target in CMakeLists.txt for header generation and implement verification and type inference for operations in toy.cpp.

--- mlir-tutorial/ex3-dialect/include/toy/ToyOps.h
+++ mlir-tutorial/ex3-dialect/include/toy/ToyOps.h
@@ -5,7 +5,11 @@
 
 // td 里面 include 的，这里也要 include 对应的 h 文件
 #include "toy/ToyDialect.h"
+#include "mlir/Interfaces/InferTypeOpInterface.h"
 #include "mlir/Interfaces/SideEffectInterfaces.h"
+#include "mlir/Interfaces/ControlFlowInterfaces.h"
+#include "mlir/Interfaces/FunctionInterfaces.h"
+#include "mlir/IR/RegionKindInterface.h"
 
 #define GET_OP_CLASSES
 #include "toy/Toy.h.inc"
--- mlir-tutorial/ex3-dialect/lib/toy.cpp
+++ mlir-tutorial/ex3-dialect/lib/toy.cpp
@@ -1,3 +1,8 @@
+#include "mlir/IR/BuiltinAttributes.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/Interfaces/CallInterfaces.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Interfaces/FunctionImplementation.h"
 #include "toy/ToyDialect.h"
 #include "toy/ToyOps.h"
 
@@ -9,9 +14,44 @@ using namespace mlir;
 using namespace toy;
 
 void ToyDialect::initialize() {
-  // 下面的代码会生成 Op 的列表，专门用来初始化
   addOperations<
 #define GET_OP_LIST
 #include "toy/Toy.cpp.inc"
   >();
+}
+
+LogicalResult SubOp::verify() {
+  if (getLhs().getType() != getRhs().getType())
+    return this->emitError() << "Lhs Type " << getLhs().getType()
+      << " not equal to rhs " << getRhs().getType();
+  return success();
+}
+
+mlir::LogicalResult ConstantOp::inferReturnTypes(
+  mlir::MLIRContext * context,
+  std::optional<mlir::Location> location,
+  Adaptor adaptor,
+  llvm::SmallVectorImpl<mlir::Type> & inferedReturnType
+) {
+  inferedReturnType.push_back(adaptor.getValueAttr().getType());
+  return mlir::success();
+}
+
+mlir::ParseResult FuncOp::parse(::mlir::OpAsmParser &parser, ::mlir::OperationState &result) {
+  auto buildFuncType = [](auto & builder, auto argTypes, auto results, auto, auto) {
+    return builder.getFunctionType(argTypes, results);
+  };
+  return function_interface_impl::parseFunctionOp(
+    parser, result, false,
+    getFunctionTypeAttrName(result.name), buildFuncType,
+    getArgAttrsAttrName(result.name), getResAttrsAttrName(result.name)
+  );
+}
+
+void FuncOp::print(mlir::OpAsmPrinter &p) {
+  // Dispatch to the FunctionOpInterface provided utility method that prints the
+  // function operation.
+  mlir::function_interface_impl::printFunctionOp(
+      p, *this, /*isVariadic=*/false, getFunctionTypeAttrName(),
+      getArgAttrsAttrName(), getResAttrsAttrName());
 }
